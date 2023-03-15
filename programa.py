a37a6631e4e9f5b40e944eff1fafd47fa4462997
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-03-15 10:15:02 -0500

    Si

---
+++ programa.py
@@ -0,0 +1,11 @@
+import random
+p = int(input("Piedra_papel_tijera: "))
+M = random.randit(1,3)
+
+if (M==1):
+    rta = "piedra"
+elif (M==2):
+    rta = "papel"
+elif (M==3):
+    rta = "tijera"
+    
