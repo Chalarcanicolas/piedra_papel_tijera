6b0ba37c8114df545605d2639bf5695e9db6640f
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-03-17 12:40:22 -0500

    se agrega el py

--- programa.py
+++ programa.py
@@ -1,11 +1,45 @@
-import random
-p = int(input("Piedra_papel_tijera: "))
-M = random.randit(1,3)
+# Piedra, papel o tijera
 
-if (M==1):
-    rta = "piedra"
-elif (M==2):
-    rta = "papel"
-elif (M==3):
-    rta = "tijera"
+import random
+
+print("--------------------------------------------")
+print("-----------Piedra_papel o tijera-----------")
+print("--------------------------------------------")
+
+print("1 es piedra")
+print("2 es papel")
+print("3 es tijera")
+
+# input
+
+j = int(input("Digite la opción: "))
+    
+# processing
+
+m = random.randint(1,3)
+
+if (j == 1):
+    if (m == 1 ):
+        print("Empate")
+    elif (m==2):
+        print("Perdiste")
+    else:
+        print("Ganaste")
+
+if (j == 2):
+    if (m == 1):
+        print("Ganaste")
+    elif (m == 2):
+        print("Empate")
+    else:
+        print ("Empate")
+    
+
+if (j == 3):
+    if ( m == 1):
+        print ("Perdiste")
+    elif ( m ==2):
+        print("Ganaste")
+    else:
+        print("Empate")
     
